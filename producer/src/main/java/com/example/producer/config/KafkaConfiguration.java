0aae62ce212cfb0f21e74039545015a2ab2640f9
Vinod John <vinodjohn56@gmail.com>
2019-12-25 03:19:03 +0200

    Style fix

--- producer/src/main/java/com/example/producer/config/KafkaConfiguration.java
+++ producer/src/main/java/com/example/producer/config/KafkaConfiguration.java
@@ -15,9 +15,7 @@ import java.util.Map;
 public class KafkaConfiguration {
     @Bean
     public ProducerFactory<String, String> producerFactory() {
-
         Map<String, Object> config = new HashMap<String, Object>();
-
         config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
         config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
